dc36b8c182ee37de8a6abdba6c169fd7b3afd850
Angosk <rodriguezr3yes@gmail.com>
2022-11-12 12:52:19 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+//* Open card description */
+document.querySelector('.cD1').addEventListener('click', () =>{
+    document.querySelector('.lforShow1').classList.toggle('show');
+})
+document.querySelector('.cD2').addEventListener('click', () =>{
+    document.querySelector('.lforShow2').classList.toggle('show');
+})
+document.querySelector('.cD3').addEventListener('click', () =>{
+    document.querySelector('.lforShow3').classList.toggle('show');
+})
+document.querySelector('.cD4').addEventListener('click', () =>{
+    document.querySelector('.lforShow4').classList.toggle('show');
+})
+document.querySelector('.cD5').addEventListener('click', () =>{
+    document.querySelector('.lforShow5').classList.toggle('show');
+})
